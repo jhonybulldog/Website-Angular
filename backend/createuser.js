39fe72adb1ea901437baf77bc162167b44fec37c
jhonny <federicodamario125@gmail.com>
2026-09-07 16:27:46 +0200

    modifica

--- backend/createuser.js
+++ backend/createuser.js
@@ -1,8 +1,8 @@
 const bcrypt = require('bcrypt');
 const db = require('./database');
 
-const username = 'admin';
-const plainPassword = 'Prova123'; // cambiala pure con quella che vuoi tu
+const username = '';
+const plainPassword = ''; // cambiala pure con quella che vuoi tu
 
 const SALT_ROUNDS = 10;
 
