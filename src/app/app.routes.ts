803bf20c35cd42ad2bd92f20742808661a3933af
jhonny <federicodamario125@gmail.com>
2026-09-11 10:51:27 +0200

    spostato dex all'interno di /pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,7 +5,7 @@ export const routes: Routes = [
   { path: '', component: HomeC },
   { path: 'admin', canActivate: [authGuard],loadComponent: () => import('./pages/admin/admin').then(m => m.Admin)},
   { path: 'login', loadComponent: () => import('./pages/admin/login/login').then(m => m.Login)},
-  { path: 'Dex' , loadComponent: () => import('./dex/dex').then(m => m.Dex)},
+  { path: 'Dex' , loadComponent: () => import('./pages/dex/dex').then(m => m.Dex)},
   //{ path: 'calc', loadComponent: () => import('./subnetcalc/subnetcalc').then(m => m.Subnetcalc) }
   { path: 'shop' , loadComponent: () => import('./pages/shop/shop').then(m => m.Shop)},
   {path: 'shop/cart', loadComponent: () => import('./pages/shop/carrello/carrello').then(m => m.Carrello)},
